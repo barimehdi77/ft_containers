7469197135b589f32a3840a11d91568662a695e3
Mehdi Bari <mbari@student.1337.ma>
2021-12-26 13:08:32 -0500

    start re-implementing vectors in c++

---
+++ containers/vector.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/26 12:04:04 by mbari             #+#    #+#             */
+/*   Updated: 2021/12/26 13:08:10 by mbari            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+#include <string>
+#include <iostream>
+
+template < class T, class Alloc = std::allocator<T> >
+class vector
+{
+	
+};
+
+# endif
