750c7f32cb6f3af326e123ce42f3b11a0eaa3495
Barimehdi77 <mbari@student.1337.ma>
2022-01-06 15:25:04 +0100

    satart implement iterator

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 12:04:04 by mbari             #+#    #+#             */
-/*   Updated: 2021/12/26 13:08:10 by mbari            ###   ########.fr       */
+/*   Updated: 2022/01/06 11:29:55 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,23 @@
 #include <string>
 #include <iostream>
 
-template < class T, class Alloc = std::allocator<T> >
-class vector
+namespace ft
 {
-	
-};
+	template < class T, class Allocator = std::allocator<T> >
+	class vector
+	{
+		protected:
+			typedef T											value_type;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::difference_type	difference_type;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
+		public:
+			typedef Allocator									allocator_type;
+			typedef typename allocator_type::size_type			size_type;
+	};
+}
+
 
 # endif
