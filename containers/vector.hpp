b0dabbc241994d45c180f0f5234fe336a95cf95f
Mehdi Bari <mbari@e2r9p10.1337.ma>
2022-01-10 15:18:24 +0100

    start working on vector

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,12 +6,14 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 12:04:04 by mbari             #+#    #+#             */
-/*   Updated: 2022/01/09 19:34:34 by mbari            ###   ########.fr       */
+/*   Updated: 2022/01/10 14:36:03 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef VECTOR_HPP
 # define VECTOR_HPP
+
+#include "iterator.hpp"
 
 #include <string>
 #include <iostream>
@@ -21,17 +23,25 @@ namespace ft
 	template < class T, class Allocator = std::allocator<T> >
 	class vector
 	{
-		protected:
+		public:
 			typedef T											value_type;
 			typedef typename allocator_type::reference			reference;
 			typedef typename allocator_type::const_reference	const_reference;
 			typedef typename allocator_type::difference_type	difference_type;
 			typedef typename allocator_type::pointer			pointer;
 			typedef typename allocator_type::const_pointer		const_pointer;
-			typedef type
-		public:
+			typedef VecIter<pointer>							iterator;
+			typedef VecIter<const_pointer>						const_iterator;
 			typedef Allocator									allocator_type;
 			typedef typename allocator_type::size_type			size_type;
+
+		private:
+			value_type											_vec;
+			size_t												_size;
+			size_t												_capacity;
+
+		public:
+			
 	};
 }
 
