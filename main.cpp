e726442949bd8884020e4ba56c96c3a9f48d0bd2
Mehdi Bari <mbari@e2r9p8.1337.ma>
2022-01-11 20:12:05 +0100

    add enable_if and finish all insert overload functions

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 18:54:40 by mbari             #+#    #+#             */
-/*   Updated: 2022/01/11 18:05:47 by mbari            ###   ########.fr       */
+/*   Updated: 2022/01/11 20:11:04 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,31 +40,89 @@
 
 
 
-/* -------------------------- |Understanding How allocator really work| --------------------------*/
+/* -------------------------- |Test for fill insert| --------------------------*/
 
 
 int main()
 {
-	std::vector<int> ft_vec(1, 1337);
-	// std::vector<int> std_vec(10);
-ft_vec.push_back(42);
-ft_vec.push_back(-42);
-std::cout << "capacity : " << ft_vec.capacity() << std::endl;
-std::cout << "size : " << ft_vec.size() << std::endl;
+	{
+        std::cout << "/* -------------------------- testing single element Insert() -------------------------- */" << std::endl;
+        ft::vector<int> ft_vec;
+        std::vector<int> std_vec;
+
+        std::cout << "ft::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "ft::size : " << std_vec.size() << std::endl;
+        std::cout << "std::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "std::size : " << std_vec.size() << std::endl;
 
 
-    ft_vec.insert(ft_vec.begin(), 77);
-    ft_vec.insert(ft_vec.begin(), 33);
-    std::vector<int>::iterator ft_it = ft_vec.begin();
-    // std::vector<int>::iterator std_it = std_vec.begin();
-    std::cout << ft_vec.capacity() << std::endl;
-    std::cout << ft_vec.size() << std::endl;
-    // ft_vec.reserve(100);
-    // std::cout << ft_vec.capacity() << std::endl;
-    // std::cout << ft_vec.size() << std::endl;
-    std::cout << *ft_it << std::endl;
-    std::cout << *(ft_it + 1) << std::endl;
-    // std::cout << std_vec.size() << std::endl;
+        ft_vec.insert(ft_vec.begin(), 77);
+        std_vec.insert(std_vec.begin(), 77);
+        std::cout << "ft::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "ft::size : " << std_vec.size() << std::endl;
+        std::cout << "std::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "std::size : " << std_vec.size() << std::endl;
 
+
+        std::cout << "ft:  " << *(ft_vec.begin()) << std::endl;
+        std::cout << "std: " << *(std_vec.begin()) << std::endl;
+        // std::cout << std_vec.size() << std::endl;
+    }
+
+    {
+        std::cout << "/* -------------------------- testing fill Insert() -------------------------- */" << std::endl;
+        ft::vector<int> ft_vec;
+        std::vector<int> std_vec;
+        std::cout << "ft::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "ft::size : " << std_vec.size() << std::endl;
+        std::cout << "std::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "std::size : " << std_vec.size() << std::endl;
+
+        ft_vec.insert(ft_vec.begin(), 10, 77);
+        std_vec.insert(std_vec.begin(), 10, 77);
+        std::cout << "ft::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "ft::size : " << std_vec.size() << std::endl;
+        std::cout << "std::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "std::size : " << std_vec.size() << std::endl;
+
+        std::cout << "ft:  ";
+        for (size_t i = 0; i < 10; i++)
+            std::cout << "|" << *(ft_vec.begin() + i) << "|" ;
+        std::cout << std::endl << "std: ";
+        for (size_t i = 0; i < 10; i++)
+            std::cout << "|" << *(std_vec.begin() + i) << "|" ;
+        std::cout << std::endl;
+    }
+
+
+    {
+        std::cout << "/* -------------------------- testing range Insert() -------------------------- */" << std::endl;
+        std::vector<int> vec;
+        vec.push_back(77);
+        vec.push_back(7);
+        vec.push_back(10);
+        vec.push_back(33);
+        ft::vector<int> ft_vec;
+        std::vector<int> std_vec;
+        std::cout << "ft::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "ft::size : " << std_vec.size() << std::endl;
+        std::cout << "std::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "std::size : " << std_vec.size() << std::endl;
+
+        ft_vec.insert(ft_vec.begin(), vec.begin(), vec.end());
+        std_vec.insert(std_vec.begin(), vec.begin(), vec.end());
+        std::cout << "ft::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "ft::size : " << std_vec.size() << std::endl;
+        std::cout << "std::capacity : " << ft_vec.capacity() << std::endl;
+        std::cout << "std::size : " << std_vec.size() << std::endl;
+
+        std::cout << "ft:  ";
+        for (size_t i = 0; i < ft_vec.size(); i++)
+            std::cout << "|" << *(ft_vec.begin() + i) << "|" ;
+        std::cout << std::endl << "std: ";
+        for (size_t i = 0; i < std_vec.size(); i++)
+            std::cout << "|" << *(std_vec.begin() + i) << "|" ;
+        std::cout << std::endl;
+    }
     return 0;
 }
