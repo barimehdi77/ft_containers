7c1451ee88cb01ebcf305b95674f8d21dde4a91c
Barimehdi77 <mbari@student.1337.ma>
2022-02-20 01:22:52 +0100

    remove pair struct from vector's utlis file

--- containers/vector/utils.hpp
+++ containers/vector/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 19:46:30 by mbari             #+#    #+#             */
-/*   Updated: 2022/02/16 20:37:15 by mbari            ###   ########.fr       */
+/*   Updated: 2022/02/20 01:22:25 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,6 @@
 #include "vector.hpp"
 namespace ft
 {
-	/*             pair Class                         */
-	template <class T1, class T2>
-	struct pair
-	{
-		typedef	T1 first_type;
-		typedef T2 second_type;
-
-		first_type	first;
-		second_type	second;
-
-		pair(): first(), second() {};
-		template<class U, class V>
-		pair (const pair<U,V>& pr): first(pr.first), second(pr.second) {};
-		pair (const first_type& a, const second_type& b): first(a), second(b) {};
-
-		pair& operator= (const pair& pr)
-		{
-			if (this != &pr)
-			{
-				this->first = pr.first;
-				this->second = pr.second;
-			}
-			return (*this);
-		};
-	};
-
-	/*             relational operators of pair                         */
-
-	template <class T1, class T2>
-	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return (lhs.first == rhs.first && lhs.second == rhs.second); };
-	template <class T1, class T2>
-	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return !(lhs == rhs); };
-	template <class T1, class T2>
-	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); };
-	template <class T1, class T2>
-	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return !(rhs<lhs); };
-	template <class T1, class T2>
-	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return rhs<lhs; };
-	template <class T1, class T2>
-	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return !(lhs<rhs); };
-
-	/*             make_pair function                         */
-	template <class T1,class T2>
-	ft::pair<T1,T2> make_pair (T1 x, T2 y) { return (ft::pair<T1,T2>(x,y)); };
-
 	template<bool Cond, class T = void> struct	enable_if				{};
 	template<class T>					struct	enable_if<true, T>		{ typedef T type; };
 
