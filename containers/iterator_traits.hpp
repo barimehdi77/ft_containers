a077f4f9479d44d7d0f615f6994413f508bd0dc7
Barimehdi77 <mbari@student.1337.ma>
2022-02-22 23:42:20 +0100

    create new file for iterator_traits

---
+++ containers/iterator_traits.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator_traits.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 22:53:57 by mbari             #+#    #+#             */
+/*   Updated: 2022/02/22 23:33:44 by mbari            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef ITERATOR_TRAITS_HPP
+#define ITERATOR_TRAITS_HPP
+
+#include <iostream>
+#include <string>
+#include <iterator>
+
+namespace ft
+{
+	template <class Iterator>
+	struct iterator_traits
+	{
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+		typedef typename Iterator::iterator_category	iterator_category;
+	};
+
+	template <class T>
+	struct iterator_traits<T*>
+	{
+		typedef ptrdiff_t						difference_type;
+		typedef T								value_type;
+		typedef T*								pointer;
+		typedef T&								reference;
+		// typedef typename T::iterator_category	iterator_category;
+	};
+
+	template <class T>
+	struct iterator_traits<const T*>
+	{
+		typedef ptrdiff_t						difference_type;
+		typedef T								value_type;
+		typedef const T*						pointer;
+		typedef const T&						reference;
+		// typedef typename iterator::iterator_category	iterator_category;
+	};
+};
+
+#endif
