4e5f971bd91fd9d7daa2ec0910fa408d2422b673
Barimehdi77 <mbari@student.1337.ma>
2022-03-11 17:55:01 +0100

    create value_comp and key_comp

--- containers/map/map.hpp
+++ containers/map/map.hpp
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 03:01:34 by mbari             #+#    #+#             */
-/*   Updated: 2022/03/11 17:37:30 by mbari            ###   ########.fr       */
+/*   Updated: 2022/03/11 17:54:28 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,6 +120,10 @@ namespace ft
 			};
 			void clear() { this->_tree.distroy(); };
 			void swap (map& x) { this->_tree.swap(x._tree); };
+
+		public: /*             Operations                         */
+			key_compare		key_comp() const	{ return (_comp); };
+			value_compare	value_comp() const	{ return (value_comp(_comp)); };
 
 		public: /*             Operations                         */
 			iterator		find (const key_type& k)		{ return (iterator(this->_tree.search(k))); };
