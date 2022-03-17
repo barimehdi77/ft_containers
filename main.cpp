77e798284818d0dff773644fcad4b2dc67f77860
Barimehdi77 <mbari@student.1337.ma>
2022-03-17 15:57:11 +0100

    updating main

--- main.cpp
+++ main.cpp
@@ -1197,3 +1197,61 @@ int main()
 // }
 
 
+        /****************************************/
+        /*                                      */
+        /*             STACK TESTS              */
+        /*                                      */
+        /****************************************/
+
+int main()
+{
+    {
+        try
+        {
+            std::cout << "========== OG ==========" << std::endl;
+            std::stack<int> st;
+            std::stack<int> st2;
+
+            std::cout << st.empty() << std::endl;
+            std::cout << st.size() << std::endl;
+            st.push(10);
+            std::cout << st.top() << std::endl;
+            st.push(20);
+            std::cout << st.top() << std::endl;
+            st.pop();
+            std::cout << st.top() << std::endl;
+            std::cout << st.empty() << std::endl;
+            std::cout << st.size() << std::endl;
+            std::cout << "comp : " << (st == st2) << std::endl;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    {
+        try
+        {
+            std::cout << "========== MINE ==========" << std::endl;
+            ft::stack<int>    st;
+            ft::stack<int>    st2;
+
+            std::cout << st.empty() << std::endl;
+            std::cout << st.size() << std::endl;
+            st.push(10);
+            std::cout << st.top() << std::endl;
+            st.push(20);
+            std::cout << st.top() << std::endl;
+            st.pop();
+            std::cout << st.top() << std::endl;
+            std::cout << st.empty() << std::endl;
+            std::cout << st.size() << std::endl;
+            std::cout << "comp : " << (st == st2) << std::endl;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+}
+
