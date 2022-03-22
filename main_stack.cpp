de871657041f3e985899a7b5ca440f46ae104b4f
Barimehdi77 <mbari@student.1337.ma>
2022-03-22 23:41:54 +0100

    adding main for stack

---
+++ main_stack.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_stack.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 22:51:30 by mbari             #+#    #+#             */
+/*   Updated: 2022/03/22 22:56:08 by mbari            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <deque>
+#include "containers/stack/stack.hpp"
+#include <stack>
+#include <stdlib.h>
+
+#ifndef	PH
+#define	PH	ft
+#endif
+
+#define COUNT (MAX_RAM / (int)sizeof(Buffer))
+
+template<typename T>
+class MutantStack : public PH::stack<T>
+{
+public:
+	MutantStack() {std::cout << "====================\n";}
+	MutantStack(const MutantStack<T>& src) { *this = src; }
+	MutantStack<T>& operator=(const MutantStack<T>& rhs)
+	{
+		this->c = rhs.c;
+		return *this;
+	}
+	~MutantStack() {}
+
+	typedef typename PH::stack<T>::container_type::iterator iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+};
+
+int main()
+{
+	PH::stack<int> stack_int;
+
+	MutantStack<char> iterable_stack;
+
+	for (char letter = 'a'; letter <= 'z'; letter++)
+		iterable_stack.push(letter);
+	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
+	{
+		std::cout << *it;
+	}
+	std::cout << std::endl;
+	PH::stack<int> t1;
+	PH::stack<int> t2;
+
+	for (int letter = 0; letter <= 5; letter++){
+		t1.push(letter);
+		t2.push(letter);
+	}
+	if (t1 == t2)
+		std::cout << "OK" << std::endl;
+	return (0);
+}
